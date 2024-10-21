be8f8e98ff2b09be38c6ddde5e7b5c16ae5aeace
Al-Zinan <pappurohan1100@gmail.com>
2024-10-22 01:53:58 +0600

    added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,6 +1,33 @@
+import Image from "next/image";
+import moduleName from "../../app/assets/reviews/ccddb3583f1.avif";
+import images2 from "../../app/assets/reviews/image2.avif";
+import images3 from "../../app/assets/reviews/image3.avif";
 import React from "react";
 
 const Blog = () => {
+  const blogData = [
+    {
+      title: "Exploring Next.js for Web Development",
+      description:
+        "Next.js is a powerful framework built on top of React. It simplifies the process of building optimized web applications...",
+      imageUrl: moduleName,
+      link: "/blog/nextjs-web-development",
+    },
+    {
+      title: "Tailwind CSS for Rapid UI Development",
+      description:
+        "Tailwind CSS is a utility-first CSS framework that allows developers to quickly build modern and responsive designs...",
+      imageUrl: images2,
+      link: "/blog/tailwind-css-development",
+    },
+    {
+      title: "React Hooks: The Essentials",
+      description:
+        "Learn the essentials of React Hooks, a powerful feature that allows you to manage state and side effects in function components...",
+      imageUrl: images3,
+      link: "/blog/react-hooks-essentials",
+    },
+  ];
   return (
     <>
       <div>
@@ -17,6 +44,8 @@ const Blog = () => {
         </div>
       </div>
       {/* title end */}
+
+      {blogData.map((blog, index) => {})}
     </>
   );
 };
