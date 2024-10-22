48e6189e6d3765763a714fbcd3e5dcb59d578a8b
Al-Zinan <pappurohan1100@gmail.com>
2024-10-22 18:58:25 +0600

    added

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import About from "@/components/About/About";
+import About from "../components/About/About.jsx";
 import { Banner } from "@/components/banner/Banner";
 import Blog from "@/components/Blog/Blog";
 import Education from "@/components/Education/Education";
